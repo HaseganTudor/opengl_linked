6beeb291385cb52b8ec155d1aece6595e9aae9e6
HaseganTudor <next15740@yahoo.com>
2025-03-22 21:20:31 +0200

    new file:   .vscode/settings.json 	new file:   CMakeLists.txt 	new file:   build 	new file:   include/KHR/khrplatform.h 	new file:   include/glad/glad.h 	new file:   src/glad.c 	new file:   src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
